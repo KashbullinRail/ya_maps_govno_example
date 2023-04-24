40634351c1c18de91a67b8f790e0a41d00b5d62c
KashbullinRail <kashbullinrail@gmail.com>
2023-04-24 21:36:37 +0300

    work in fragment_blank.xml

--- app/src/main/java/com/example/ya_maps_govno_example/BlankFragment.kt
+++ app/src/main/java/com/example/ya_maps_govno_example/BlankFragment.kt
@@ -1,26 +1,64 @@
 package com.example.ya_maps_govno_example
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-
+import androidx.fragment.app.Fragment
+import com.example.ya_maps_govno_example.databinding.FragmentBlankBinding
+import com.yandex.mapkit.Animation
+import com.yandex.mapkit.MapKitFactory
+import com.yandex.mapkit.geometry.Point
+import com.yandex.mapkit.map.CameraPosition
+import com.yandex.mapkit.mapview.MapView
 
 class BlankFragment : Fragment() {
 
+    private var _binding: FragmentBlankBinding? = null
+    private val binding get() = _binding!!
+
+    private val TARGET_LOCATION = Point(55.763740752167465, 37.40582814075713)
+    private val mapView: MapView by lazy { requireActivity().findViewById(R.id.mapview) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+        MapKitFactory.setApiKey("05ee083d-246d-421d-9956-0a76757d842a")
+        MapKitFactory.initialize(requireContext().applicationContext)
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-
-        return inflater.inflate(R.layout.fragment_blank, container, false)
+        _binding = FragmentBlankBinding.inflate(inflater, container, false)
+        val view = binding.root
+        return view
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        mapView.getMap().move(
+            CameraPosition(TARGET_LOCATION, 17.0f, 0.0f, 0.0f),
+            Animation(Animation.Type.SMOOTH, 5f),
+            null
+        )
+    }
+
+    override fun onStop() {
+        mapView.onStop()
+        MapKitFactory.getInstance().onStop()
+        super.onStop()
+    }
+
+    override fun onStart() {
+        super.onStart()
+        MapKitFactory.getInstance().onStart()
+        mapView.onStart()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
 
 }
--- app/src/main/java/com/example/ya_maps_govno_example/MainActivity.kt
+++ app/src/main/java/com/example/ya_maps_govno_example/MainActivity.kt
@@ -11,36 +11,10 @@ import com.yandex.mapkit.mapview.MapView
 
 class MainActivity : AppCompatActivity() {
 
-    private val TARGET_LOCATION = Point(55.763740752167465, 37.40582814075713)
-    private var mapView: MapView? = null
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        // Now MapView can be created.
-        // Now MapView can be created.
-        MapKitFactory.setApiKey("05ee083d-246d-421d-9956-0a76757d842a")
-        MapKitFactory.initialize(this)
-        setContentView(R.layout.map)
-
-        mapView = findViewById<View>(R.id.mapview) as MapView
-        // And to show what can be done with it, we move the camera to the center of Saint Petersburg.
-        // And to show what can be done with it, we move the camera to the center of Saint Petersburg.
-        mapView?.getMap()?.move(
-            CameraPosition(TARGET_LOCATION, 17.0f, 0.0f, 0.0f),
-            Animation(Animation.Type.SMOOTH, 5f),
-            null
-        )
+        setContentView(R.layout.activity_main)
+        supportFragmentManager.beginTransaction().add(R.id.container, BlankFragment()).commit()
     }
 
-    override fun onStop() {
-        mapView?.onStop()
-        MapKitFactory.getInstance().onStop()
-        super.onStop()
-    }
-
-    override fun onStart() {
-        super.onStart()
-        MapKitFactory.getInstance().onStart()
-        mapView?.onStart()
-    }
 }
